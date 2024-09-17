487c23b6cb10f46c35c5ef5ed71fbda298d13bba
heloskanaaziza <heloskanaaziza@gmail.com>
2024-09-17 08:18:53 +0700

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// mendapatkan lokasi saat ini (latitude dan longitude) pengguna.
+import { Geolocation } from '@capacitor/geolocation';
+// membuat peta dan menambahkan titik (marker) di peta.
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import Graphic from '@arcgis/core/Graphic';
+import Point from '@arcgis/core/geometry/Point';
+import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
 
 @Component({
+  // Mendefinisikan komponen
   selector: 'app-home',
+  // Mengarahkan ke file HTML yang digunakan sebagai template di komponen
   templateUrl: 'home.page.html',
+  // Mengarahkan ke file CSS
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor() { }
 
+  private latitude: number | any ;
+  private longitude: number | any ;
+
+  public async ngOnInit() {
+
+    // lokasi sekarang
+    // Mengambil lokasi pengguna
+    const position = await Geolocation.getCurrentPosition();
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+
+    // Membuat peta dengan basemap
+    const map = new Map({
+      basemap: "topo-vector"
+    });
+
+    // Membuat tampilan peta, atur zoom dan pusat tampilan
+    const view = new MapView({
+      container: "container",
+      map: map,
+      zoom:  15,
+      center: [this.longitude, this.latitude]
+    });
+
+    // Membuat titik (marker)
+    const point = new Point({
+      longitude: this.longitude,
+      latitude: this.latitude
+    });
+
+    const markerSymbol = new SimpleMarkerSymbol({
+      color: [226, 119, 40],  // warna marker
+      outline: {
+        color: [255, 255, 255], // warna outline
+        width: 1
+      }
+    });
+
+    const pointGraphic = new Graphic({
+      geometry: point,
+      symbol: markerSymbol
+    });
+
+    // Menambahkan marker ke peta
+    view.graphics.add(pointGraphic);
+  }
 }
